4844bf1996c103d0215b649693cbf8f9f1ce468f
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2019-07-02 14:04:21 +0300

    Create class for storing vehicle stop status

---
+++ src/main/java/fi/hsl/transitdata/vehicleposition/application/StopStatusProcessor.java
@@ -0,0 +1,66 @@
+package fi.hsl.transitdata.vehicleposition.application;
+
+import com.google.transit.realtime.GtfsRealtime;
+import fi.hsl.common.hfp.proto.Hfp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StopStatusProcessor {
+    private final Map<String, VehicleStopStatus> vehicleStopStatus = new HashMap<>(1000);
+
+    public VehicleStopStatus getStopStatus(Hfp.Data hfpData) {
+        return vehicleStopStatus.compute(hfpData.getTopic().getUniqueVehicleId(), (uniqueVehicleId, previousStopStatus) -> processStopStatus(previousStopStatus, hfpData));
+    }
+
+    private VehicleStopStatus processStopStatus(VehicleStopStatus previousStopStatus, Hfp.Data hfpData) {
+        if (previousStopStatus == null || hfpData.getTopic().getEventType() == Hfp.Topic.EventType.PDE || hfpData.getTopic().getEventType() == Hfp.Topic.EventType.PAS) {
+            //Set StopStatus to IN_TRANSIT_TO when a vehicle departs from a stop or passes through a stop
+            return new VehicleStopStatus(hfpData.getTopic().getNextStop(), GtfsRealtime.VehiclePosition.VehicleStopStatus.IN_TRANSIT_TO);
+        }
+
+        if (hfpData.getTopic().getEventType() == Hfp.Topic.EventType.DUE) {
+            //Set StopStatus to INCOMING_AT when a vehicle is just about to arrive to a stop
+            return new VehicleStopStatus(hfpData.getTopic().getNextStop(), GtfsRealtime.VehiclePosition.VehicleStopStatus.INCOMING_AT);
+        }
+
+        if (hfpData.getTopic().getEventType() == Hfp.Topic.EventType.ARS) {
+            //Set StopStatus to STOPPED_AT when a vehicle has arrived to a stop
+            return new VehicleStopStatus(hfpData.getTopic().getNextStop(), GtfsRealtime.VehiclePosition.VehicleStopStatus.STOPPED_AT);
+        }
+
+        //If a vehicle was arriving to a stop and the stop id in payload has not changed, return previous status
+        if (previousStopStatus.stopStatus == GtfsRealtime.VehiclePosition.VehicleStopStatus.INCOMING_AT &&
+                hfpData.getPayload().hasStop() &&
+                String.valueOf(hfpData.getPayload().getStop()).equals(previousStopStatus.stopId)) {
+            return previousStopStatus;
+        }
+
+        if (previousStopStatus.stopStatus == GtfsRealtime.VehiclePosition.VehicleStopStatus.STOPPED_AT) {
+            if (hfpData.getPayload().hasStop() && String.valueOf(hfpData.getPayload().getStop()).equals(hfpData.getTopic().getNextStop())) {
+                //If next_stop has not changed in the topic, the vehicle is still at a stop
+                return previousStopStatus;
+            } else {
+                //If next_stop has changed, the vehicle is in transit to the next stop
+                return new VehicleStopStatus(hfpData.getTopic().getNextStop(), GtfsRealtime.VehiclePosition.VehicleStopStatus.IN_TRANSIT_TO);
+            }
+        }
+
+        //If vehicle has reached its final stop, remove it from the list
+        if ("EOL".equals(hfpData.getTopic().getNextStop())) {
+            return null;
+        }
+
+        return new VehicleStopStatus(hfpData.getTopic().getNextStop(), GtfsRealtime.VehiclePosition.VehicleStopStatus.IN_TRANSIT_TO);
+    }
+
+    public static class VehicleStopStatus {
+        public final String stopId;
+        public final GtfsRealtime.VehiclePosition.VehicleStopStatus stopStatus;
+
+        private VehicleStopStatus(String stopId, GtfsRealtime.VehiclePosition.VehicleStopStatus stopStatus) {
+            this.stopId = stopId;
+            this.stopStatus = stopStatus;
+        }
+    }
+}
---
+++ src/test/java/fi/transitdata/vehicleposition/application/StopStatusProcessorTest.java
@@ -0,0 +1,73 @@
+package fi.transitdata.vehicleposition.application;
+
+import com.google.transit.realtime.GtfsRealtime;
+import fi.hsl.common.hfp.proto.Hfp;
+import fi.hsl.transitdata.vehicleposition.application.StopStatusProcessor;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class StopStatusProcessorTest {
+    private Hfp.Data generateHfpData(Hfp.Topic.EventType eventType, String nextStop, String payloadStop) {
+        Hfp.Payload.Builder payload = Hfp.Payload.newBuilder();
+            payload.setSchemaVersion(1);
+            payload.setTst("");
+            payload.setTsi(0);
+            if (payloadStop != null) {
+                payload.setStop(Integer.parseInt(payloadStop));
+            }
+
+        return Hfp.Data.newBuilder()
+                .setSchemaVersion(1)
+                .setTopic(Hfp.Topic.newBuilder()
+                        .setSchemaVersion(1)
+                        .setReceivedAt(0)
+                        .setTopicPrefix("hfp")
+                        .setTopicVersion("v2")
+                        .setJourneyType(Hfp.Topic.JourneyType.journey)
+                        .setTemporalType(Hfp.Topic.TemporalType.ongoing)
+                        .setOperatorId(1)
+                        .setVehicleNumber(1)
+                        .setUniqueVehicleId("1/1")
+                        .setEventType(eventType)
+                        .setNextStop(nextStop))
+                .setPayload(payload)
+                .build();
+    }
+
+    @Test
+    public void testCalculateStopStatus() {
+        StopStatusProcessor calculator = new StopStatusProcessor();
+
+        StopStatusProcessor.VehicleStopStatus stopStatus1 = calculator.getStopStatus(generateHfpData(Hfp.Topic.EventType.VP, "1", null));
+        assertEquals(GtfsRealtime.VehiclePosition.VehicleStopStatus.IN_TRANSIT_TO, stopStatus1.stopStatus);
+        assertEquals("1", stopStatus1.stopId);
+
+        StopStatusProcessor.VehicleStopStatus stopStatus2 = calculator.getStopStatus(generateHfpData(Hfp.Topic.EventType.DUE, "1", "1"));
+        assertEquals(GtfsRealtime.VehiclePosition.VehicleStopStatus.INCOMING_AT, stopStatus2.stopStatus);
+        assertEquals("1", stopStatus2.stopId);
+
+        StopStatusProcessor.VehicleStopStatus stopStatus3 = calculator.getStopStatus(generateHfpData(Hfp.Topic.EventType.VP, "1", "1"));
+        assertEquals(GtfsRealtime.VehiclePosition.VehicleStopStatus.INCOMING_AT, stopStatus3.stopStatus);
+        assertEquals("1", stopStatus3.stopId);
+
+        StopStatusProcessor.VehicleStopStatus stopStatus4 = calculator.getStopStatus(generateHfpData(Hfp.Topic.EventType.ARS, "1", "1"));
+        assertEquals(GtfsRealtime.VehiclePosition.VehicleStopStatus.STOPPED_AT, stopStatus4.stopStatus);
+        assertEquals("1", stopStatus4.stopId);
+
+        StopStatusProcessor.VehicleStopStatus stopStatus5 = calculator.getStopStatus(generateHfpData(Hfp.Topic.EventType.PDE, "2", "1"));
+        assertEquals(GtfsRealtime.VehiclePosition.VehicleStopStatus.IN_TRANSIT_TO, stopStatus5.stopStatus);
+        assertEquals("2", stopStatus5.stopId);
+
+        StopStatusProcessor.VehicleStopStatus stopStatus6 = calculator.getStopStatus(generateHfpData(Hfp.Topic.EventType.VP, "2", null));
+        assertEquals(GtfsRealtime.VehiclePosition.VehicleStopStatus.IN_TRANSIT_TO, stopStatus6.stopStatus);
+        assertEquals("2", stopStatus6.stopId);
+
+        StopStatusProcessor.VehicleStopStatus stopStatus7 = calculator.getStopStatus(generateHfpData(Hfp.Topic.EventType.PAS, "3", "2"));
+        assertEquals(GtfsRealtime.VehiclePosition.VehicleStopStatus.IN_TRANSIT_TO, stopStatus7.stopStatus);
+        assertEquals("3", stopStatus7.stopId);
+
+        assertNull(calculator.getStopStatus(generateHfpData(Hfp.Topic.EventType.VP, "EOL", null)));
+    }
+}
