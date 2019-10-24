27def46669c09ae55fbd87171da40d0d549be631
Jaakko Malkki <32361480+mjaakko@users.noreply.github.com>
2019-10-24 14:49:21 +0300

    Revert "feat/position_from_hfp_events"

--- src/main/java/fi/hsl/transitdata/vehicleposition/application/utils/StopLocationProvider.java
+++
@@ -1,7 +0,0 @@
-package fi.hsl.transitdata.vehicleposition.application.utils;
-
-import java.util.Optional;
-
-public interface StopLocationProvider {
-    Optional<double[]> getStopLocation(String stopId) throws Exception;
-}
