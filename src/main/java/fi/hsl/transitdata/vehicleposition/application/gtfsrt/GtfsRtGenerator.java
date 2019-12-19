28989a9c003915d5df628d583b1d60d2d15393c1
Jaakko Malkki <32361480+mjaakko@users.noreply.github.com>
2019-12-19 10:12:33 +0200

    Revert "feat/position_from_hfp_events"

--- src/main/java/fi/hsl/transitdata/vehicleposition/application/gtfsrt/GtfsRtGenerator.java
+++ src/main/java/fi/hsl/transitdata/vehicleposition/application/gtfsrt/GtfsRtGenerator.java
@@ -4,30 +4,17 @@ import com.google.transit.realtime.GtfsRealtime;
 import fi.hsl.common.hfp.proto.Hfp;
 import fi.hsl.transitdata.vehicleposition.application.StopStatusProcessor;
 import fi.hsl.transitdata.vehicleposition.application.utils.RouteIdNormalizer;
-import fi.hsl.transitdata.vehicleposition.application.utils.StopLocationProvider;
-import fi.hsl.transitdata.vehicleposition.application.utils.TunnelStopLocationProvider;
 
 import java.util.Optional;
 
 import static fi.hsl.transitdata.vehicleposition.application.utils.TimeUtils.getStartTime;
 
 public class GtfsRtGenerator {
-    private static final StopLocationProvider STOP_LOCATION_PROVIDER = new TunnelStopLocationProvider();
-
     private GtfsRtGenerator() {}
 
     public static Optional<GtfsRealtime.VehiclePosition> generateVehiclePosition(Hfp.Data hfpData, StopStatusProcessor.StopStatus stopStatus) {
-        //If the vehicle is stopped at a stop, we can use that location of that stop for vehicle position
-        //This is required to display location of trains in Kehärata tunnel
-        Optional<double[]> maybeStopLocation;
-        try {
-            maybeStopLocation = stopStatus.stopStatus == GtfsRealtime.VehiclePosition.VehicleStopStatus.STOPPED_AT ? STOP_LOCATION_PROVIDER.getStopLocation(stopStatus.stopId) : Optional.empty();
-        } catch (Exception e) {
-            maybeStopLocation = Optional.empty();
-        }
-
-        //Ignore messages where the vehicle has no location or location cannot be determined from stop status
-        if ((!hfpData.getPayload().hasLat() || !hfpData.getPayload().hasLong()) && !maybeStopLocation.isPresent()) {
+        //Ignore messages where the vehicle has no location
+        if (!hfpData.getPayload().hasLat() || !hfpData.getPayload().hasLong()) {
             return Optional.empty();
         }
 
@@ -40,12 +27,9 @@ public class GtfsRtGenerator {
             vp.setStopId(stopStatus.stopId);
         }
 
-        Optional<Double> stopLatitude = maybeStopLocation.map(location -> location[0]);
-        Optional<Double> stopLongitude = maybeStopLocation.map(location -> location[1]);
-
         vp.setPosition(GtfsRealtime.Position.newBuilder()
-                .setLatitude((float)(hfpData.getPayload().hasLat() ? hfpData.getPayload().getLat() : stopLatitude.get()))
-                .setLongitude((float)(hfpData.getPayload().hasLong() ? hfpData.getPayload().getLong() : stopLongitude.get()))
+                .setLatitude((float) hfpData.getPayload().getLat())
+                .setLongitude((float) hfpData.getPayload().getLong())
                 .setSpeed((float) hfpData.getPayload().getSpd())
                 .setBearing(hfpData.getPayload().getHdg())
                 .setOdometer(hfpData.getPayload().getOdo()));
