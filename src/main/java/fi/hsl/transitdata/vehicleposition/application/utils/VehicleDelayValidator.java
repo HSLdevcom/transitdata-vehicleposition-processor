03f2692c7eaf92463163d6d87dafaf1b5001cada
Janne Löfhjelm <janne.lofhjelm@gmail.com>
2023-03-16 12:17:24 +0200

    Add logging and feature-switch

--- src/main/java/fi/hsl/transitdata/vehicleposition/application/utils/VehicleDelayValidator.java
+++ src/main/java/fi/hsl/transitdata/vehicleposition/application/utils/VehicleDelayValidator.java
@@ -9,13 +9,28 @@ public class VehicleDelayValidator {
     private static final Logger log = LoggerFactory.getLogger(VehicleDelayValidator.class);
 
     private final long maximumDelay;
+    private final boolean isEnabled;
 
     public VehicleDelayValidator(long maximumDelay) {
         this.maximumDelay = maximumDelay;
+        this.isEnabled = false;
+    }
+
+    public VehicleDelayValidator(long maximumDelay, boolean isEnabled) {
+        this.maximumDelay = maximumDelay;
+        this.isEnabled = isEnabled;
     }
 
     public boolean validateDelay(Hfp.Data hfpData) {
+        if (!isEnabled) {
+            return true;
+        }
 
-        return true;
+        if (hfpData.getPayload().getDl() < maximumDelay) {
+            return true;
+        }
+
+        log.warn("Vehicle {} had delay (dl) too big (vehicle: {}, Maximum delay allowed: {})", hfpData.getTopic().getUniqueVehicleId(), hfpData.getPayload().getTsi(), maximumDelay);
+        return false;
     }
 }
